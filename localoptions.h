311d9ab2328217517d8d542fbc1c68c7b92685f0
Ben Gardner <bgardner@wabtec.com>
2017-01-09 11:31:00 -0600

    LOS178: Add localoptions.h

---
+++ localoptions.h
@@ -0,0 +1,16 @@
+#define DROPBEAR_SVR_AGENTFWD 1
+
+#define DROPBEAR_CLI_AGENTFWD 1
+
+#define DROPBEAR_PIDFILE "/run/dropbear.pid"
+
+#define DSS_PRIV_FILENAME "/etc/dropbear/dropbear_dss_host_key"
+#define RSA_PRIV_FILENAME "/etc/dropbear/dropbear_rsa_host_key"
+#define ECDSA_PRIV_FILENAME "/etc/dropbear/dropbear_ecdsa_host_key"
+#define MOTD_FILENAME "/etc/motd"
+
+#define DROPBEAR_PATH_SSH_PROGRAM "/bin/dbclient"
+
+#define DROPBEAR_LISTEN_BACKLOG 2
+
+#define READONLY_DEVFS 1
