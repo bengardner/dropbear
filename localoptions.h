6a0f32e094287727018e9c224d0b051187d97a4b
Ben Gardner <bgardner@wabtec.com>
2017-06-07 09:16:55 -0500

    localoptions.h: remove default #defines

--- localoptions.h
+++ localoptions.h
@@ -4,12 +4,6 @@
 
 #define DROPBEAR_PIDFILE "/run/dropbear.pid"
 
-#define DSS_PRIV_FILENAME "/etc/dropbear/dropbear_dss_host_key"
-#define RSA_PRIV_FILENAME "/etc/dropbear/dropbear_rsa_host_key"
-#define ECDSA_PRIV_FILENAME "/etc/dropbear/dropbear_ecdsa_host_key"
-#define MOTD_FILENAME "/etc/motd"
-
-#define DROPBEAR_PATH_SSH_PROGRAM "/bin/dbclient"
 
 #define DROPBEAR_LISTEN_BACKLOG 2
 
